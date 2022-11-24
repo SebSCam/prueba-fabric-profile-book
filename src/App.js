21ad0701f7b2c5d3c6f040fa349275c38d651ba7
SebSCam <sebas9903@outlook.com>
2022-11-24 13:27:09 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const url = "/profiles";
+
+  const [picture, setPicture] = useState(null);
+  const [gender, setGender] = useState(null);
+  const [name, setName] = useState(null);
+  const [mail, setMail] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPicture(data[0].picture.large));
+  }, []);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPicture(data[0].name.first + data[0].name.last));
+  }, []);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPicture(data[0].gender));
+  }, []);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPicture(data[0].email));
+  }, []);
+
+  const saveProfile = (data) => {
+    axios.post(url + "/save", data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <p>{!picture ? "Waiting profile" : picture}</p>
+        <img src={picture}></img>
+        <p>{name}</p>
+        <p>{gender}</p>
+        <p>{mail}</p>
+        <button onClick={saveProfile}>Save as Favorite</button>
+      </div>
     </div>
   );
 }
